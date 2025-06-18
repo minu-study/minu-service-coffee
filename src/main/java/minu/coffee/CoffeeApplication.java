f523877f78f16c42407fca8b694c6bee7de5a744
minu <min9288@gmail.com>
2025-06-19 00:11:50 +0900

    refactor: 상위 패키지 추가로 인해 경로 변경

---
+++ src/main/java/minu/coffee/CoffeeApplication.java
@@ -0,0 +1,37 @@
+package minu.coffee;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+@Slf4j
+@SpringBootApplication
+@ComponentScan({"coffee.*"})
+public class CoffeeApplication {
+
+    public static void main(String[] args) {
+        log.info("ConciergeApplication Service Start args : {}", Arrays.stream(args).toList());
+        ApplicationContext ctx = SpringApplication.run(CoffeeApplication.class, args);
+        DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+
+    }
+
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        log.info("Service Start TimeZone : {} / Time : {} / Locale : {}", TimeZone.getDefault().getID(), new Date(), Locale.getDefault());
+    }
+
+
+}
