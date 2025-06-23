8432f58bbff85d7826e4e0e5655e57c16fc88bb9
minu <min9288@gmail.com>
2025-06-23 10:25:36 +0900

    refactor: response 맵핑 유틸 클래스 위치 변경에 따른 코드 수정

--- src/main/java/minu/coffee/api/auth/controller/AuthController.java
+++ src/main/java/minu/coffee/api/auth/controller/AuthController.java
@@ -4,20 +4,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import minu.coffee.api.auth.service.AuthService;
 import minu.coffee.auth.dto.LoginDto;
-import minu.coffee.common.filter.Exception.AppException;
-import minu.coffee.common.filter.Exception.ErrorCode;
 import minu.coffee.common.model.ApiResponse;
-import minu.coffee.common.model.TokenMember;
-import minu.coffee.common.util.ResponseUtil;
-import minu.coffee.config.security.JwtUtil;
-import org.springframework.http.HttpStatus;
+import minu.coffee.common.util.CommonUtil;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,14 +22,14 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<ApiResponse> register(@RequestBody @Valid LoginDto.Register.Request param) {
         authService.register(param);
-        return ResponseUtil.ConvertResponse();
+        return CommonUtil.ConvertResponse();
     }
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse> login(@RequestBody LoginDto.Login.Request param) {
 
         LoginDto.Login.Response response = authService.login(param);
-        return ResponseUtil.ConvertResponse(response);
+        return CommonUtil.ConvertResponse(response);
 
     }
 }
