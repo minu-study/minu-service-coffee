8ced52cc799cb0e42091c1c19ceaa5ae36b36a8a
minu <min9288@gmail.com>
2025-06-23 10:25:57 +0900

    feat: 상품 목록 조회 API

---
+++ src/main/java/minu/coffee/api/product/controller/ProductController.java
@@ -0,0 +1,26 @@
+package minu.coffee.api.product.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import minu.coffee.api.product.service.ProductService;
+import minu.coffee.common.model.ApiResponse;
+import minu.coffee.common.util.CommonUtil;
+import minu.coffee.product.ProductDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/list")
+    public ResponseEntity<ApiResponse> getList() {
+        ProductDto.GetProducts.Response response = productService.getProducts();
+        return CommonUtil.ConvertResponse(response);
+    }
+
+}
---
+++ src/main/java/minu/coffee/api/product/service/ProductService.java
@@ -0,0 +1,36 @@
+package minu.coffee.api.product.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import minu.coffee.common.model.TokenInfo;
+import minu.coffee.common.util.CommonUtil;
+import minu.coffee.product.ProductDto;
+import minu.coffee.product.repository.ProductQueryRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductQueryRepository productQueryRepository;
+
+    @Transactional(readOnly = true)
+    public ProductDto.GetProducts.Response getProducts() {
+
+        TokenInfo tokenInfo = CommonUtil.getTokenInfo();
+
+        List<ProductDto.ProductSummaryView> productSummaryViewList
+                = productQueryRepository.getProductSummaryView(tokenInfo.getShopInfoId(), LocalDateTime.now());
+
+        return ProductDto.GetProducts.Response.builder()
+                .list(productSummaryViewList)
+                .build();
+    }
+
+
+}
